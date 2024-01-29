4c3174ef735a211713d4e2791e2dddccdd5ae192
Dan Desktop <d95tan@gmail.com>
2024-01-29 20:01:21 +0800

    feat: create individual cards for items

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,34 +1,70 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, ButtonGroup, TextField } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
-export default function ItemCard() {
+export default function ItemCard({ item, changeQuantity }) {
+  const handleChange = (e) => {
+    changeQuantity(item?.name, parseInt(e.target.value));
+  };
+
+  const handleAdd = () => {
+    changeQuantity(item?.name, item?.quantity + 1);
+    // console.log(bill);
+  };
+
+  const handleRemove = () => {
+    changeQuantity(item?.name, item?.quantity - 1);
+  };
+
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
+      <CardActionArea
+        onClick={handleAdd}
+        disabled={item?.inventory > item?.quantity ? false : true}
+      >
         <CardMedia
           component="img"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
+          image={`../../../public/${item?.name}.png`}
+          alt={item?.name}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {item?.name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            Inventory: {item?.inventory}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
+      <ButtonGroup variant="outlined" aria-label="outlined button group">
+        <Button
+          onClick={handleRemove}
+          disabled={item?.quantity > 0 ? false : true}
+        >
+          <RemoveIcon />
         </Button>
-      </CardActions>
+        <TextField
+          id="outlined-basic"
+          label="Quantity"
+          type="number"
+          min={0}
+          max={item?.inventory}
+          value={item?.quantity}
+          variant="outlined"
+          onChange={handleChange}
+        />
+        <Button
+          onClick={handleAdd}
+          disabled={item?.inventory > item?.quantity ? false : true}
+        >
+          <AddIcon />
+        </Button>
+      </ButtonGroup>
     </Card>
   );
 }
